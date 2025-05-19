b5508b11aa6fef851abda7e6df1f0f2ebeb0a8ef
roemello.mccoy <roemello.mccoy22@gmail.com>
2025-05-18 19:11:22 -0600

    Projectile creates random splat on collision

--- Source/GAM415/GAM415Projectile.h
+++ Source/GAM415/GAM415Projectile.h
@@ -22,6 +22,12 @@ class AGAM415Projectile : public AActor
 	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category = Movement, meta = (AllowPrivateAccess = "true"))
 	UProjectileMovementComponent* ProjectileMovement;
 
+	UPROPERTY(EditAnywhere)
+	UStaticMeshComponent* ballMesh;
+
+	UPROPERTY(EditAnywhere)
+	UMaterial* baseMat;
+
 public:
 	AGAM415Projectile();
 
